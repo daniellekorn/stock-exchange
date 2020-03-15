f19fca50940fe23b1f181ee63e2f197e6a84a019
Danielle Korn <daniella.korn@gmail.com>
2020-03-15 18:38:10 +0200

    loader added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const searchButton = document.getElementById("searchButton");
 const resultChart = document.getElementById("resultChart");
 const searchText = document.getElementById("searchText");
+const loader = document.getElementById("loader");
 
 function clearHistory() {
 	let child = resultChart.lastElementChild;
@@ -21,14 +22,19 @@ async function searching() {
 	let data = await response.json();
 
 	for (let i = 0; i < data.length; i++) {
-		let newResult = document.createElement("div");
-		newResult.innerHTML = `${data[i].name} (${data[i].symbol})`;
+		let newResult = document.createElement("a");
+		let name = data[i].name;
+		let symbol = data[i].symbol;
+		newResult.classList.add("result");
+		newResult.innerHTML = `${name} (${symbol})`;
+		newResult.href = "/company.html?symbol=" + symbol;
 		resultChart.appendChild(newResult);
 	}
+	loader.classList.replace("show", "hide");
 }
 
 searchButton.addEventListener("click", () => {
+	loader.classList.replace("hide", "show");
 	userInput = searchText.value;
-	console.log(userInput);
 	searching();
 });
