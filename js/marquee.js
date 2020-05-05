fcc5ce5eb0261d7f6b0bcfd8df13913c6d76c3f5
Danielle Korn <daniella.korn@gmail.com>
2020-05-05 09:57:23 +0300

    marquee edits: no more hard html

--- js/marquee.js
+++ js/marquee.js
@@ -1,27 +1,6 @@
 class Marquee {
-	constructor(parent, id) {
+	constructor(parent) {
 		this.parent = parent;
-		this.id = id;
-	}
-
-	createMarquee() {
-		const marqueeBody = document.createElement("div");
-		marqueeBody.classList.add("marquee");
-		marqueeBody.id = this.id;
-		this.getData().then(items => {
-			const allListings = items.map(item => {
-				if (item.changes.toString().includes("-")) {
-					return `<li class="marquee-list"><span>${item.indexName}</span> <span class="negative">$${item.price}</span></li>`;
-				} else {
-					return `<li class="marquee-list"><span>${item.indexName}</span> <span class="positive">$${item.price}</span></li>`;
-				}
-			});
-			const ul = document.createElement("ul");
-			ul.classList.add("marquee-content", "flexible");
-			ul.insertAdjacentHTML("beforeend", allListings.join(""));
-			marqueeBody.appendChild(ul);
-			this.parent.insertAdjacentElement("afterbegin", marqueeBody);
-		});
 	}
 
 	async getData() {
@@ -31,5 +10,33 @@ class Marquee {
 		let marqueeData = await marqueeResponse.json();
 		let stocks = marqueeData.majorIndexesList;
 		return stocks;
+	}
+
+	createMarquee() {
+		const marqueeBody = document.createElement("div");
+		marqueeBody.classList.add("marquee");
+		marqueeBody.id = "marquee";
+		const ul = document.createElement("ul");
+		ul.classList.add("marquee-content", "flexible");
+		this.getData().then((items) => {
+			items.map((item) => {
+				const listItem = document.createElement("li");
+				listItem.className = "marquee-list";
+				const childName = document.createElement("span");
+				childName.textContent = item.indexName;
+				const childPrice = document.createElement("span");
+				childPrice.textContent = item.price;
+				if (item.changes.toString().includes("-")) {
+					childPrice.className = "negative";
+				} else {
+					childPrice.className = "positive";
+				}
+				listItem.appendChild(childName);
+				listItem.appendChild(childPrice);
+				ul.appendChild(listItem);
+			});
+			marqueeBody.appendChild(ul);
+			this.parent.insertAdjacentElement("afterbegin", marqueeBody);
+		});
 	}
 }
